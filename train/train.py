ebbf21c7d49bf2ce78573ccc9c6bb94e0f93d0db
narain pattabhiraman <narainp@outlook.com>
2020-07-22 09:41:39 +0530

    multi model training

--- train/train.py
+++ train/train.py
@@ -12,7 +12,7 @@ valid_aug = A.Compose([A.Normalize(), A.pytorch.transforms.ToTensorV2()])
 
 df = pd.read_csv('../data/train.csv')
 
-model_name = 'efficientnet-b0'
+model_names = ['efficientnet-b0', 'efficientnet-b2', 'efficientnet-b3']
 folds = 5
 
 early_stopping_cb = EarlyStopping('score',
@@ -24,20 +24,20 @@ early_stopping_cb = EarlyStopping('score',
 checkpoint_callback = ModelCheckPoint(filepath = 'models',
         monitor = 'score'
         mode = 'max')
-
-for bi, (train_ids, val_ids) in enumerate(kfs.split(df, df['class'])):
-    train_df, val_df = df.iloc[train_ids, :], df.iloc[val_ids, :]
-    train_ds, val_ds = ImageDataset(train_df, train_aug), ImageDataset(val_df, val_aug)
-    train_dl = DataLoader(train_ds, pin_memory=True, shuffle=True, batch_size=32, num_workers=4)
-    val_dl = DataLoader(val_ds, pin_memory=True, batch_size=32, num_workers=4)
-    model = Model(model_name)
-    trainer = Trainer(gpus=1,
-            checkpoint_callback = 1.0,
-            early_stop_callback = early_stopping_cb,
-            max_epochs = 20,
-            num_sanity_val_steps = 10
-            )
-    trainer.fit(model, train_dl, val_dl)
-    model = Model.load_from_checkpoint(early_stopping_cb.best_model_path)
-    torch.save(model.state_dict(), f'model_{bi}')
+for model_name in model_names:
+    for bi, (train_ids, val_ids) in enumerate(kfs.split(df, df['class'])):
+        train_df, val_df = df.iloc[train_ids, :], df.iloc[val_ids, :]
+        train_ds, val_ds = ImageDataset(train_df, train_aug), ImageDataset(val_df, val_aug)
+        train_dl = DataLoader(train_ds, pin_memory=True, shuffle=True, batch_size=32, num_workers=4)
+        val_dl = DataLoader(val_ds, pin_memory=True, batch_size=32, num_workers=4)
+        model = Model(model_name)
+        trainer = Trainer(gpus=1,
+                checkpoint_callback = 1.0,
+                early_stop_callback = early_stopping_cb,
+                max_epochs = 20,
+                num_sanity_val_steps = 10
+                )
+        trainer.fit(model, train_dl, val_dl)
+        model = Model.load_from_checkpoint(early_stopping_cb.best_model_path)
+        torch.save(model.state_dict(), f'model_{bi}')
 
